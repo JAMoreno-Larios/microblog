ae117e0e0f4e308ea304a8ea6bb348866ca86ee1
José Agustín Moreno Larios <agustin90m@gmail.com>
2025-09-26 07:57:54 -0600

    Work up to Chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'José'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Diana'},
+            'body': 'Que fuerte la película de Kimetsu no Yaiba'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user,
+                           posts=posts)
