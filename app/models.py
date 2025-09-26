5d97752d60bd4b2c5866192063438554fc00cad9
José Agustín Moreno Larios <agustin90m@gmail.com>
2025-09-26 15:26:19 -0600

    Added password handling methods to 'User' class in models.py

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Initialize SQLAlchemy instance
 db = SQLAlchemy()
@@ -31,6 +32,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
