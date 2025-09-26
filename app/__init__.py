7ef0aedaf78af6a3c7a346687cfc21af156f304e
José Agustín Moreno Larios <agustin90m@gmail.com>
2025-09-26 14:11:57 -0600

    Added database support with Flask-SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -2,23 +2,15 @@
 Microblog initialization file
 """
 
-from flask import Flask, Blueprint
+from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .routes import routes_bp
+from .models import db, User, Post
 
-# app = Flask(__name__)
-# app.config.from_object(Config)
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-#
-# from app import routes, models  # Prevents circular import
 
 # Initialize extensions outside the application factory
-db = SQLAlchemy()
 migrate = Migrate()
-
 
 def create_app(test_config=None):
     # Create and configure the Flask app
@@ -27,5 +19,4 @@ def create_app(test_config=None):
     db.init_app(app)
     migrate.init_app(app, db)
     app.register_blueprint(routes_bp)
-    # import .models
     return app
--- config.py
+++ config.py
@@ -1,4 +1,18 @@
+"""
+Configuration file.
+We declare environment variables that are processed
+with python-dotenv
+"""
+
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
+    """
+    Configuration class for our Flask app
+    """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'adivina-adivinador'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'sqlite:///' + os.path.join(
+        basedir, 'app.db')
