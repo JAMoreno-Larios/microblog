e0761497e3a7e1d6f926696af1906a55a5c1b70a
José Agustín Moreno Larios <agustin90m@gmail.com>
2025-09-27 07:58:57 -0600

    Added avatars via Gravatar

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ import sqlalchemy.orm as so
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, LoginManager
+from hashlib import md5
 
 # Initialize SQLAlchemy instance
 db = SQLAlchemy()
@@ -42,6 +43,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return \
+            f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 
 class Post(db.Model):
