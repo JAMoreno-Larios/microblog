739c7aaf32ec157e25a9f475e67a361b9d1f53a5
José Agustín Moreno Larios <agustin90m@gmail.com>
2025-09-27 11:16:07 -0600

    Added logging to file support

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from .routes import routes_bp
 from .errors import errors_bp
 from .models import db, login, User, Post
@@ -37,8 +38,8 @@ def create_app(test_config=None):
     First section enables email logging for errors; second,
     logs to a text file.
     """
-    # Enable email logging when the application is running without debug mode
     if not app.debug:
+        # Email configuration
         if app.config['MAIL_SERVER']:
             auth = None
             if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -62,5 +63,24 @@ def create_app(test_config=None):
             )
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
+
+        # Logging to a file
+        # Checks if file exists; if not, we create one
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log',
+                                           maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(
+            logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'
+            )
+        )
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
 
     return app
