27383a852aaf3c68dbfb22848e874c96403d292a
José Agustín Moreno Larios <agustin90m@gmail.com>
2025-10-08 12:56:50 -0600

    Can add new users via API

--- app/api/users.py
+++ app/api/users.py
@@ -45,7 +45,31 @@ def get_following(id):
 
 @users_bp.route('/', methods=['POST'])
 def create_user():
-    pass
+    data = request.get_json()
+    if 'username' not in data\
+        or 'email' not in data\
+            or 'password' not in data:
+        return bad_request(
+                    "must include username, email and password fields"
+            )
+    if db.session.scalar(
+        sa.select(User).where(
+                User.username == data['username']
+            )
+    ):
+        return bad_request('please use a different username')
+    if db.session.scalar(
+            sa.select(User).where(
+                User.email == data['email']
+            )
+    ):
+        return bad_request('please use a different email address')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    return user.to_dict(), 201, {'Location': url_for('api.users.get_user',
+                                                     id=user.id)}
 
 
 @users_bp.route('<int:id>', methods=['PUT'])
